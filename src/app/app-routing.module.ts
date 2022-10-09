32b2babf9dc092a8b229f81a70f13d22e4300ea5
Sourya Mishra <sourya.mishra111@gmail.com>
2022-10-09 17:34:58 +0530

    change path for hero detail route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
   { path: "heroes", component: HeroesComponent },
   { path: "dashboard", component: DashboardComponent },
   { path: "", redirectTo: "dashboard", pathMatch: "full" },
-  { path: "detail/:id", component: HeroDetailComponent },
+  { path: "heroes/:id", component: HeroDetailComponent },
 ];
 
 @NgModule({
