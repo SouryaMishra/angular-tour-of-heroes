ab2839f1d489ec9f9ad6cb6b282f387886a5bfd8
Sourya Mishra <sourya.mishra111@gmail.com>
2022-10-09 21:39:52 +0530

    add and update routes, add link and active class

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { CrisisListComponent } from "./crisis-list/crisis-list.component";
 import { DashboardComponent } from "./dashboard/dashboard.component";
 import { HeroDetailComponent } from "./hero-detail/hero-detail.component";
 import { HeroesComponent } from "./heroes/heroes.component";
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 
 const routes: Routes = [
   { path: "heroes", component: HeroesComponent },
   { path: "dashboard", component: DashboardComponent },
-  { path: "", redirectTo: "dashboard", pathMatch: "full" },
+  { path: "crisis-center", component: CrisisListComponent },
   { path: "heroes/:id", component: HeroDetailComponent },
+  { path: "", redirectTo: "dashboard", pathMatch: "full" },
+  { path: "**", component: PageNotFoundComponent },
 ];
 
 @NgModule({
