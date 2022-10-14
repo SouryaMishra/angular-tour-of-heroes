3849976e09d7737aa0c70105a0a74341a39c5919
Sourya Mishra <sourya.mishra@educationsoftwaresolutions.co.uk>
2022-10-14 10:50:35 +0530

    add transform and style

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -20,11 +20,15 @@ export const slideInAnimation = trigger("routeAnimation", [
         width: "100%",
       }),
     ]),
-    query(":enter", [style({ left: "-100%" })]),
+    query(":enter", [style({ transform: "translateX(-100%)" })]),
     query(":leave", animateChild()),
     group([
-      query(":leave", [animate("300ms ease-out", style({ left: "100%" }))]),
-      query(":enter", [animate("300ms ease-out", style({ left: "0%" }))]),
+      query(":leave", [
+        animate("300ms ease-out", style({ transform: "translateX(100%)" })),
+      ]),
+      query(":enter", [
+        animate("300ms ease-out", style({ transform: "translateX(0%)" })),
+      ]),
     ]),
     query(":enter", animateChild()),
   ]),
